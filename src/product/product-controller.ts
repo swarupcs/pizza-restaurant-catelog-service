c99cc84b8dc156eb5eb3f7c259467e19899228ad
Swarup Das <swarupcs@gmail.com>
2026-02-14 13:22:37 +0000

    add tenant access check

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -7,6 +7,8 @@ import { ProductService } from "./product-service";
 import { Product } from "./product-types";
 import { FileStorage } from "../common/types/storage";
 import { UploadedFile } from "express-fileupload";
+import { AuthRequest } from "../common/types";
+import { Roles } from "../common/constants";
 
 export class ProductController {
     constructor(
@@ -64,6 +66,24 @@ export class ProductController {
 
         const { productId } = req.params;
 
+        if ((req as AuthRequest).auth.role === Roles.MANAGER) {
+            const product = await this.productService.getProduct(productId);
+            if (!product) {
+                return next(createHttpError(404, "Product not found"));
+            }
+
+            const tenant = (req as AuthRequest).auth.tenant;
+
+            if (product.tenantId !== String(tenant)) {
+                return next(
+                    createHttpError(
+                        403,
+                        "You are not allowed to access this product",
+                    ),
+                );
+            }
+        }
+
         let imageName: string | undefined;
         let oldImage: string | undefined;
 
@@ -91,7 +111,7 @@ export class ProductController {
             isPublish,
         } = req.body;
 
-        const product = {
+        const productToUpdate = {
             name,
             description,
             priceConfiguration: JSON.parse(priceConfiguration as string),
@@ -102,7 +122,7 @@ export class ProductController {
             image: imageName ? imageName : (oldImage as string),
         };
 
-        await this.productService.updateProduct(productId, product);
+        await this.productService.updateProduct(productId, productToUpdate);
 
         res.json({ id: productId });
     };
